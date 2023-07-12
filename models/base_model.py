518a5aa6af5f73318854fd661813b03fa3e44121
Frida <amina.amer.9898@gmail.com>
2023-07-12 15:12:48 -0700

    Recreate an instance with the dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,20 @@ import uuid
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """class initialization"""
+        if kwargs:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == "created_at" or "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setatr(self, key, value)
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """print representation of the class"""
