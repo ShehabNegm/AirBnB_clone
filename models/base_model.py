3a588fb3e50fc28806139323a1f4c3e5b4499891
Frida <amina.amer.9898@gmail.com>
2023-07-12 15:25:39 -0700

    Recreate an instance with the dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setatr(self, key, value)
+                setattr(self, key, value)
 
         else:
             self.id = str(uuid.uuid4())
