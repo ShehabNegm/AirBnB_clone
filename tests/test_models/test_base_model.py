6f9aee57f1a04550b493a2fc4c7fff35e5b2d343
ShehabNegm <shebonegm@gmail.com>
2023-07-13 11:59:03 -0700

    update unittestes for basemodel after adding kwargs and args

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -38,6 +38,29 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(dic_object['__class__'],
                          self.base_model.__class__.__name__)
 
+    def test_args_kwargs(self):
+        dic_object = self.base_model.to_dict()
+        my_new_model = BaseModel(**dic_object)
+        self.assertIsInstance(my_new_model.id, str)
+        self.assertEqual(my_new_model.id, self.base_model.id)
+        self.assertIsInstance(my_new_model.created_at, datetime)
+        self.assertIsInstance(my_new_model.updated_at, datetime)
+
+    def test_no_args_kwargs(self):
+        baseModel = BaseModel()
+        baseModel.name = "cool"
+        baseModel.num = 10
+        self.assertIsInstance(baseModel.id, str)
+        self.assertEqual(baseModel.name, "cool")
+        self.assertEqual(baseModel.num, 10)
+        self.assertEqual(baseModel.created_at, baseModel.updated_at)
+        self.assertIsInstance(baseModel.created_at, datetime)
+        self.assertIsInstance(baseModel.updated_at, datetime)
+        baseModel.save()
+        self.assertNotEqual(baseModel.created_at, baseModel.updated_at)
+        dic_object = baseModel.to_dict()
+        self.assertEqual(dic_object["__class__"], "BaseModel")
+
 
 if __name__ == '__main__':
     unittest.main()
