f4a711c07e9fdbae765062f47d2d56c7f6b2de35
ShehabNegm <shebonegm@gmail.com>
2023-07-14 02:51:42 -0700

    add create method of console.py

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Defines HBNBCommand class"""
-
 import cmd
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = "(hbnb) "
+    classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the console"""
@@ -19,6 +21,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Handles empty lines"""
         pass
+
+    def do_create(self, arg):
+        """create new instance of arg and save it so json file"""
+
+        if not arg:
+            print("** class name missing **")
+
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+
+        else:
+            instance = eval(arg)()
+            instance.save()
+            print(instance.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
