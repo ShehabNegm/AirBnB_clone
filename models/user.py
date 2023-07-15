6fd457dcfb5950a082beb6317309861974ac1538
Frida <amina.amer.9898@gmail.com>
2023-07-15 07:26:33 -0700

    Edit User class

--- models/user.py
+++ models/user.py
@@ -5,9 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class"""
-    def __init__(self, email="", password="", first_name="", last_name=""):
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
