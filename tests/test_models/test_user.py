9e5d2462b7475e0ec1299700dd227eb1f196c8fd
ShehabNegm <shebonegm@gmail.com>
2023-07-17 12:44:22 -0700

    update test_user.py to include class arttibutes

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,35 +18,35 @@ class TestUser(unittest.TestCase):
         self.user.email = "test@test.com"
         self.user.password = "123"
 
-    def test_user_isinstance(self):
+    def test_isinstance(self):
         self.assertIsInstance(User(), BaseModel)
 
-    def test_user_created_at(self):
+    def test_created_at(self):
         self.assertIsInstance(self.user.created_at, datetime)
 
-    def test_user_updated_at(self):
+    def test_updated_at(self):
         self.assertIsInstance(self.user.updated_at, datetime)
 
-    def test_user_id(self):
+    def test_id(self):
         self.assertIsInstance(self.user.id, str)
 
-    def test_user_id_2(self):
+    def test_id_2(self):
         id = self.user.id
         self.assertEqual(id, self.user.id)
 
-    def test_user_save_update(self):
+    def test_save_update(self):
         update1 = self.user.updated_at
         self.user.save()
         update2 = self.user.updated_at
         self.assertNotEqual(update1, update2)
 
-    def test_user_to_dict_keys(self):
+    def test_to_dict_keys(self):
         keys = ['id', 'updated_at', 'created_at', '__class__', 'first_name',
                 'last_name', 'email', 'password']
         dic_object = self.user.to_dict()
         self.assertCountEqual(keys, dic_object.keys())
 
-    def test_user_to_dict_values(self):
+    def test_to_dict_values(self):
         dic_object = self.user.to_dict()
         my_new_model = User(**dic_object)
         self.assertIsInstance(my_new_model.id, str)
@@ -54,25 +54,25 @@ class TestUser(unittest.TestCase):
         self.assertIsInstance(my_new_model.created_at, datetime)
         self.assertIsInstance(my_new_model.updated_at, datetime)
 
-    def test_user_email(self):
+    def test_email(self):
         email = "test@test.com"
         self.user.email = email
         self.assertIsInstance(self.user.email, str)
         self.assertEqual(self.user.email, "test@test.com")
 
-    def test_user_passwrod(self):
+    def test_passwrod(self):
         password = "12345"
         self.user.password = password
         self.assertIsInstance(self.user.password, str)
         self.assertEqual(self.user.password, "12345")
 
-    def test_user_first_name(self):
+    def test_first_name(self):
         first_name = "lol"
         self.user.first_name = first_name
         self.assertIsInstance(self.user.first_name, str)
         self.assertEqual(self.user.first_name, "lol")
 
-    def test_user_last_name(self):
+    def test_last_name(self):
         last_name = "nah"
         self.user.last_name = last_name
         self.assertIsInstance(self.user.last_name, str)
